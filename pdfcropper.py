70c62c61b9d2987a673d87ea493cbcc52524554c
Jasper Zanjani <jasper@Jaspers-MacBook-Pro.local>
2019-04-14 15:45:20 -0400

    Initial commit

---
+++ pdfcropper.py
@@ -0,0 +1,62 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+path = "C:/Users/jaspe/Documents/Books and Courses/"
+filename = "gawk"
+fn=f'{path}{filename}-cropped.pdf'
+
+# Margins in inches
+# --All pages
+top, right, bottom, left= 0.5, 1, 1, 1
+
+# --Odd pages
+oddLeft   =left
+oddTop    =top
+oddRight  =right
+oddBottom =bottom
+# --Even pages
+evenLeft  =left
+evenTop   =top
+evenRight =right
+evenBottom=bottom
+
+# Convert inches to points
+oddLeft   *=72
+oddTop    *=72
+oddRight  *=72
+oddBottom *=72
+evenLeft  *=72
+evenRight *=72
+evenTop   *=72
+evenBottom*=72
+
+# pagesToBreak = [18,  ]
+pagesToExclude = []
+
+if __name__ == "__main__":
+  with open(f'{path}{filename}.pdf','rb') as r:
+    global orig
+    orig = PdfFileReader(r)
+    pages = orig.getNumPages()
+    pagesToPrint = pages
+
+    
+    
+    out = PdfFileWriter()
+    for i in range(pagesToPrint):
+      p = orig.getPage(i)
+      y=float(p.mediaBox.getUpperRight_y())
+      x=float(p.mediaBox.getUpperRight_x())
+      # print(x,y)
+      if (i+1)%2==0: # even pages
+        p.cropBox.lowerLeft=(evenLeft,evenBottom)
+        p.cropBox.upperRight= (x-evenRight,y-evenTop)
+      else:          # odd pages
+        p.cropBox.lowerLeft=(oddLeft,oddBottom)
+        p.cropBox.upperRight= (x-oddRight,y-oddTop)
+      if i not in pagesToExclude:
+        out.addPage(p)
+      else:
+        continue
+    with open(fn, "wb") as outf:
+      out.write(outf)
+      print(f'PDF cropped to {fn}')
