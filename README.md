fee345395271315ce0c4b7f7472c018ff12ffad4
jasper-zanjani <jasper.zanjani@gmail.com>
2020-06-08 00:15:19 -0400

    Refactoring

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # PDFCropper
 **PDFCropper** is a simple command-line utility that crops the margins around PDFs for the purpose of printing as efficiently as possible.
 
-The script accepts a single argument (the PDF file to be cropped) and any one of several optional switch arguments that correspond to the margins typical of that publisher: `examref` ("Exam Ref" series published by Microsoft), `manning`, `nostarch`, and `packtpub`. Support for custom margins coming soon...
+The script accepts a single argument (the PDF file to be cropped) and a single option `--format`. 
+The values passed to `--format` determine margins dimensions typical to that publisher: `examref` ("Exam Ref" series published by Microsoft), `manning`, `nostarch`, and `packtpub`. 
+Support for custom margins coming soon...
 
 #### Example
 ```sh
-python pdfcropper.py ~/doc.pdf --packtpub
+python pdfcropper.py ~/doc.pdf --format packtpub
 ```
--- pdfcropper.py
+++ pdfcropper.py
@@ -1,15 +1,22 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import os, sys, click
 
+pagesToExclude = []
+
+def crop(p, top, right, bottom, left):
+  height = float(p.mediaBox.getUpperRight_y())
+  width = float(p.mediaBox.getUpperRight_x())
+  p.cropBox.upperLeft=(left, height - bottom)
+  p.cropBox.lowerRight= (width - right, bottom)
+  return p
+
+
 @click.command()
-@click.option('--examref', is_flag=True)
-@click.option('--packtpub', is_flag=True)
-@click.option('--manning', is_flag=True)
-@click.option('--nostarch', is_flag=True)
+@click.option('--format', type=click.Choice(['examref','packtpub','manning','nostarch'], case_sensitive=False))
 @click.argument('filename')
-def setmargins(examref, packtpub, manning, nostarch, filename):
+def setmargins(format, filename):
   oddLeft, oddTop, oddRight, oddBottom, evenLeft, evenTop, evenRight, evenBottom, top, right, bottom, left = 0,0,0,0,0,0,0,0,0,0,0,0
-  if examref:
+  if format.lower()=='examref':
     top, right, bottom, left= 1.5,0,1.75,0
     oddLeft   =1.75
     oddTop    =top
@@ -19,7 +26,7 @@ def setmargins(examref, packtpub, manning, nostarch, filename):
     evenTop   =top
     evenRight =1.75
     evenBottom=bottom
-  if packtpub:
+  elif format.lower()=='packtpub':
     top, right, bottom, left = 0.625, 0.75, 0.625, 0.75
     oddLeft   =left
     oddTop    =top
@@ -29,7 +36,7 @@ def setmargins(examref, packtpub, manning, nostarch, filename):
     evenTop   =top
     evenRight =right
     evenBottom=bottom
-  if manning:
+  elif format.lower()=='manning':
     top, right, bottom, left = 0.375, 0.875,0.5,0.375
     oddLeft   =0.5
     oddTop    =top
@@ -39,20 +46,11 @@ def setmargins(examref, packtpub, manning, nostarch, filename):
     evenTop   =top
     evenRight =right
     evenBottom=bottom
-  if nostarch:
+  elif format.lower()=='nostarch':
     top, right, bottom, left= 0.5,0.625,0.25,0.625
-    oddLeft   =left
-    oddTop    =top
-    oddRight  =right
-    oddBottom =bottom
-    evenLeft  =left
-    evenTop   =top
-    evenRight =right
-    evenBottom=bottom
-
-
-
-  
+    oddLeft,oddTop,oddRight,oddBotom,evenLeft,evenTop,evenRight,evenBottom = left,top,right,bottom,left,top,right,bottom
+  else:
+    oddLeft, oddTop, oddRight, oddBottom, evenLeft, evenTop, evenRight, evenBottom = top, right, bottom, left, top, right, bottom, left
   oddLeft   *=72
   oddTop    *=72
   oddRight  *=72
@@ -62,36 +60,37 @@ def setmargins(examref, packtpub, manning, nostarch, filename):
   evenTop   *=72
   evenBottom*=72
 
+  # Read file
+  # Might want to break this out into its own function, if I can figure out how to get the thing working outside of the `with` block!
   path = os.path.dirname(filename)
   filename = os.path.basename(filename)
   fo=f'{path}/{filename}-cropped.pdf'
   with open(f'{path}/{filename}','rb') as r:
     global orig
     orig = PdfFileReader(r)
-    pages = orig.getNumPages()
-    pagesToPrint = pages
+    pagesToPrint = orig.getNumPages()
     
     out = PdfFileWriter()
     for i in range(pagesToPrint):
-      p = orig.getPage(i)
-      y=float(p.mediaBox.getUpperRight_y())
-      x=float(p.mediaBox.getUpperRight_x())
-      # print(x,y)
-      if (i+1)%2==0: # even pages
-        p.cropBox.lowerLeft=(evenLeft,evenBottom)
-        p.cropBox.upperRight= (x-evenRight,y-evenTop)
-      else:          # odd pages
-        p.cropBox.lowerLeft=(oddLeft,oddBottom)
-        p.cropBox.upperRight= (x-oddRight,y-oddTop)
       if i not in pagesToExclude:
-        out.addPage(p)
+        p = orig.getPage(i)
+        newpage = p
+        if (i+1)%2==0: # even pages
+          newpage = crop(p,evenTop,evenRight,evenBottom,evenLeft)
+        else:          # odd pages
+          newpage = crop(p, oddTop, oddRight, oddBottom, oddLeft)
+        out.addPage(newpage)
       else:
         continue
+
+    # Write file
+    # Might need to break this out into its own function
+    # def write_file(filename):
     with open(fo, "wb") as outf:
       out.write(outf)
       print(f'PDF cropped to {fo}')
 
-pagesToExclude = []
 
 if __name__ == "__main__":
   setmargins()
+
